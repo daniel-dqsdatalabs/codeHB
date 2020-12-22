23336dce1a9504ae8c1611091e34ef53369fd3c3
daniel-dqsdatalabs <daniel@dqsdatalabs.com>
2020-12-22 06:51:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## Desafio Técnico CodeHB
+
+#### Solução
+
+A solução proposta foi desenvolvida em python rodando no azure functions. Os arquivos json baixados da API estão sendo armazendos em um blob storage e a execução ocorre a cada 24 horas sendo disparada pelo data factory.
+
+O código foi desenvolvido tendo como premissa a legibilidade, visando permitir a uma pessoa com conhecimento básico em python entender seu objetivo.
+
+Optei por utilizar uma função recursiva para realizar a paginação dos dados, isso torna a solução mais enxuta e evita o uso de estruturas de repetição que na minha opinião são um anti-pattern quando manipulamos grande volumes de dados (computação distribuida).
+
+Para gerar o json final, utilizei uma técnica chamada flattening, que visa nivelar um objeto JSON complexo (diversos níveis) e gerar um objeto com apenas um nível de profundidade.
+
+Será gerado um único arquivo para cada dia, a execução ocorrerá às 00:00 AM.
+
+#### Artefatos Utilizados
+
+- Resource Group: 
+    - codehb
+- Storage Account
+    - nome: codehb
+    - container: results/YYYY/MM/DD
+- Function App
+    - nome: codehb-function
+    - function: daily_trigger
+    - application insights: codehb-function
+- Data Factory: 
+    - nome: datafactory-codehb
+    - pipeline: pipeline_download_escolas
+    - linked service: LF_AF_ESCOLAS
+- App Service Plan:
+    - nome: BrazilSouthLinuxDynamicPlan
+Preparado por:
+Daniel Queiroz
--- daily_trigger/__init__.py
+++ daily_trigger/__init__.py
@@ -1,14 +1,102 @@
-import datetime
+
+#==============================================================================
+# filename          : __init__.py
+# description       : download json file
+# author            : daniel
+# email             : daniel@dqsdatalabs.com
+# date              : 19/12/2020
+# version           : 0.01
+#==============================================================================
+
+import os
+import sys
+import time
+import json
 import logging
+import requests
+import itertools
+import functools
+import pandas as pd
+from datetime import datetime
 
 import azure.functions as func
+from azure.storage.blob import ContainerClient
+
+DAY = str(datetime.today().day).zfill(2)
+YEAR = str(datetime.today().year).zfill(4)
+MONTH = str(datetime.today().month).zfill(2)
+
+BASE_URL = 'https://dadosabertos.poa.br'
+NEXT_URL = '/api/3/action/datastore_search?offset=0&resource_id=5579bc8e-1e47-47ef-a06e-9f08da28dec8'
+
+APP_STR = os.environ['AzureWebJobsStorage']
+
+def process_request(next_url, result):
+    '''
+        recursivelly iterate over pages
+    '''
+    headers = { 
+        'User-Agent': 'curl/7.58.0',
+        'accept': 'application/json',
+        'Content-Type': 'application/json; charset=UTF-8'
+    }
+    
+    logging.info('url: %s', next_url)
+    r = requests.get(next_url, headers=headers).json()
+    
+    if len(r["result"]["records"]) > 0:
+        result.append(r["result"]["records"])
+        process_request(BASE_URL + r["result"]["_links"]["next"], result)
+        
+    return result
 
 
-def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+def save_json_file(result_json):
+    '''
+        save the json file
+    ''' 
+    container_client = ContainerClient.from_connection_string(
+        conn_str=APP_STR,
+        container_name='results/{}/{}/{}'.format(YEAR, MONTH, DAY)
+    )
+    
+    filename = "lista_escolas_{ts}.json".format(
+        ts=datetime.now().timestamp()
+    )
+    
+    container_client.upload_blob(
+        name=filename,
+        data=(json.dumps(result_json, ensure_ascii=False)),
+        blob_type="BlockBlob"
+    )
 
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
+    logging.info('Json salvo com sucesso...')
+        
+        
+def flatten_results(result_list):
+    '''
+        flattening the results
+    '''
+    return functools.reduce(lambda x,y: x + y, result_list)
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+def process_results():
+    '''
+        process results
+    '''
+    result = []
+    r = process_request(BASE_URL + NEXT_URL, result)
+    if r is not None: result.append(flatten_results(r))
+    result_json =  {"escolas":  flatten_results(result)}
+    save_json_file(result_json)
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    start = time.perf_counter()
+    logging.info('processamento iniciado em: %s', datetime.now())
+    try:
+        process_results()
+        return json.dumps({'status_code': 200})
+    except Exception:
+        return func.HttpResponse(Exception.message, status_code=500)
+    duration = time.perf_counter() - start
+    logging.info('tempo de processamento: %s', duration)
